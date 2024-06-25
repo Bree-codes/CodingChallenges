1cf1e2184f7d72d33ab3af054b6674be2e8e2773
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-25 07:55:23 +0300

    Working on deleting an element from an array

--- ArrayDeletion/src/Main.java
+++ ArrayDeletion/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int[] arr = {10,16,27,38,2,51,79};
+        int num = 27;
+
+        Solutions solutions = new Solutions();
+        int k = solutions.searchElement(arr,num);
+
+        System.out.println("The number " + num + " is at index: " + k);
     }
 }
---
+++ ArrayDeletion/src/Solutions.java
@@ -0,0 +1,16 @@
+public class Solutions {
+
+    public int searchElement(int[] arr,int num){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == num){
+                return i;
+            }
+        }
+        System.out.println("Element Not Found!");
+        return -1;
+    }
+
+    public static void deleteElement(int[] arr,int num){
+        if()
+    }
+}
