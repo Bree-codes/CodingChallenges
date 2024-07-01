38bdc25ac37bca1e80de0fa43f59a7b5341a5baa
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-01 11:56:22 +0300

    Working on binary search

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         int[] arr = {10,20,30,40,50,60,70,80,90,100,110};
         int target = 100;
 
+        Solution solution = new Solution();
     }
 }
---
+++ BinarySearch/src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+
+    public int binarySearch(int arr[] ,int target){
+
+
+        return -1;
+    }
+}
