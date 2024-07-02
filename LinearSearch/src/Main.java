1a8ec29e696b4f3555bfd81c445fed4aff2a4231
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-02 09:24:26 +0300

    Linear search

--- LinearSearch/src/Main.java
+++ LinearSearch/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = {10,12,8,67,45,90};
+        int target = 90;
+
+        int index = linearSearch(arr,target);
+        System.out.println("The element was found at index: " + index);
+    }
+
+    public static int linearSearch(int[] arr, int target){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
     }
 }
