a31315445300b2771fc55aac6f31ca27fbaee282
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-02 09:03:22 +0300

    Binary search that prints the index of where an element would be if its not initially present in the array

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {10,20,30,40,50,60,70,80,90,100,110};
-        int target = 55;
+        //int[] arr = {50,45,37,31,29,26,13};
+        int target = 13;
 
         Solution solution = new Solution();
         int index = solution.binarySearch(arr,target);
-        System.out.println("The element is at  index: " + index);
+        System.out.println("The element is at or would have been at index: " + index);
     }
 }
