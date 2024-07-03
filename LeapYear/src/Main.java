5f632c004671f3a02b587251a069a35aeb1ceae7
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-03 06:15:24 +0300

    Done

--- LeapYear/src/Main.java
+++ LeapYear/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        int year = 2000;
+        int year = 2010;
         Solution solution = new Solution();
         String result = solution.checkingLeapYear(year);
 
