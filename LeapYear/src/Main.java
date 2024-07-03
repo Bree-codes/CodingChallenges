078266e1db47b62092422b0f0c15ba3e0d7df85a
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-03 06:05:34 +0300

    Done but can be better..

--- LeapYear/src/Main.java
+++ LeapYear/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        int year = 1901;
+        int year = 2000;
         Solution solution = new Solution();
         String result = solution.checkingLeapYear(year);
 
--- LeapYear/src/Solution.java
+++ LeapYear/src/Solution.java
@@ -1,8 +1,8 @@
 public class Solution {
 
     public String checkingLeapYear(int year){
-        if(year % 4 == 0){
-            return "This is a leap year";
+        if(year % 4 == 0 && year % 400 == 0){
+            return "This is a leap year..";
         }
         return "This is not a leap year!";
     }
