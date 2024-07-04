3a7ca8e76068fae808400139a2b96bc124bf7d47
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-04 13:13:47 +0300

    Bubble sort algo

--- BubbleSort/src/Main.java
+++ BubbleSort/src/Main.java
@@ -9,13 +9,19 @@ public class Main {
     }
     public static int[] bubbleSort(int[] arr){
         for(int i = 0; i< arr.length; i++){
-            for(int j = 1; j< arr.length; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for(int j = 0; j< arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
+
+                for(int n : arr){
+                    System.out.print(n+"\t");
+                }
+                System.out.println("\n");
             }
         }
+        return arr;
     }
 }
