8cdf74e3999a3760553306c19aea05347464d626
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-08 06:35:32 +0300

    Working on Merge sort algo

--- MergeSort/src/Main.java
+++ MergeSort/src/Main.java
@@ -2,11 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {8, 2, 5, 3, 4, 7, 6, 1};
 
-        Solution solution = new Solution();
-        solution.mergeSort(array);
+       Solution.mergeSort(array);
+
         for (int j : array) {
             System.out.print(j + " ");
-
         }
     }
 }
