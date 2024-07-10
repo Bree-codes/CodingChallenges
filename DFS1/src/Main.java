474677fb14218a34ccff0dae6138091845467bca
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-10 06:27:01 +0300

    DFS

---
+++ DFS1/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 2);
+
+        graph.print();
+
+        graph.depthFirstSearch(0);
+
+    }
+}
