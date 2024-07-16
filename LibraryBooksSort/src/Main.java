9aec88de7176829d6e1cb27f2ed072ac5887cc29
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-16 18:14:03 +0300

    Library books sort

--- LibraryBooksSort/src/Main.java
+++ LibraryBooksSort/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int noOfRandomNums = 10;
+
+        for(int i = 0 ; i < noOfRandomNums; i++){
+            int randomNums = random.nextInt(100);
+            System.out.print(randomNums + ",");
+        }
+
 
 
     }
