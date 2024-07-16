bcff153bc564b12c0e269fd5359042a6b63f901f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-16 18:35:43 +0300

    Library books sort

--- LibraryBooksSort/src/Main.java
+++ LibraryBooksSort/src/Main.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Generating Random Numbers.
         Random random = new Random();
         int noOfRandomNums = 10;
 
+        int[] randomNumsArray = new int[noOfRandomNums];
+
         for(int i = 0 ; i < noOfRandomNums; i++){
-            int randomNums = random.nextInt(100);
-            System.out.print(randomNums + ",");
+          randomNumsArray[i] = random.nextInt(100);
         }
+        System.out.print(Arrays.toString(randomNumsArray));
 
-
+        //Creating an instance of Solution class
+        Solution solution = new Solution();
+        solution.mergeSort(randomNumsArray);
+        //solution.binarySearch(randomNumsArray,45);
 
     }
 }
--- LibraryBooksSort/src/Solution.java
+++ LibraryBooksSort/src/Solution.java
@@ -2,5 +2,79 @@ public class Solution {
     //Here we have the 2 main methods merge sorting the random numbers
     //then binary searching for the specific num we need
 
+    //merge sort
+    public void mergeSort(int[] arr){
+
+        int length = arr.length;
+
+        if (length <= 1) return;
+
+        int mid = length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+
+
+        for(int i = 0 ; i < length ; i++){
+            if(i < mid){
+                leftArray[i] = arr[i];
+            }
+            else{
+                rightArray[i - mid] = arr[i];
+            }
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(arr,rightArray,leftArray);
+    }
+    public void merge(int[] arr,int[]leftArray,int[] rightArray){
+
+        int leftLength = leftArray.length;
+        int rightLength = rightArray.length;
+        int i = 0,j = 0,k=0;
+        while(i <leftLength && j< rightLength ){
+            if(leftArray[i] < rightArray[j]){
+                arr[k] = leftArray[i];
+                i++;
+            }
+            else{
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while(i< leftLength){
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        while(j< rightLength){
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+
+    }
+
+    //binary search
+    public void binarySearch(int[] arr, int target){
+
+        int low = 0, high = arr.length - 1;
+
+        while(low <= high){
+
+            int middle = low + (high - low) / 2;
+            int value = arr[middle];
+            System.out.println(value);
+
+            if(target > value){
+                low = middle + 1;
+            }
+            else if(target < value){
+                high = middle - 1;
+            }
+            else return;
+        }
+    }
 
 }
