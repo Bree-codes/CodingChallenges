91aaa469171f3e77ac90d1c1cebe63d9508efced
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-18 19:57:22 +0300

    GCD Algo with Recursion

--- GcdWithRecursion/src/Main.java
+++ GcdWithRecursion/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 48,b = 18;
+
+        Solution solution = new Solution();
+        int gcd = solution.greatestCommonDivisor(a,b);
+        System.out.println("The gcd of "+ a +" and "+ b + " is: "+ gcd);
     }
 }
