c34c3d148eef5a1d1712d8f0b3ebd2a19338e006
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-19 07:49:29 +0300

    Commit message >>>>> added a recursive method to find the GCD of an array of numbers. <<<<<

--- GcdWithRecursion/src/Main.java
+++ GcdWithRecursion/src/Main.java
@@ -1,9 +1,17 @@
+import java.math.MathContext;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 48,b = 18;
+        int a = 120,b = 90;
 
         Solution solution = new Solution();
         int gcd = solution.greatestCommonDivisor(a,b);
         System.out.println("The gcd of "+ a +" and "+ b + " is: "+ gcd);
+
+        int[] array = {120, 90};
+
+        int GDC = solution.GCD(array, 2, 1);
+
+        System.out.println("GDC : "+GDC);
     }
 }
