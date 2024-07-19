eaa5d1f515aadea187baa8a2db6a12639614ae70
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-19 08:08:24 +0300

    Commit message >>>>> intensive test done. <<<<<

--- GcdWithRecursion/src/Main.java
+++ GcdWithRecursion/src/Main.java
@@ -1,15 +1,20 @@
 import java.math.MathContext;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int a = 120,b = 90;
 
         Solution solution = new Solution();
-        int gcd = solution.greatestCommonDivisor(a,b);
-        System.out.println("The gcd of "+ a +" and "+ b + " is: "+ gcd);
+       /* int gcd = solution.greatestCommonDivisor(a,b);
+        System.out.println("The gcd of "+ a +" and "+ b + " is: "+ gcd);*/
+        int len = 1000;
+        int[] array = new int[len];
+        Random random = new Random();
 
-        int[] array = {120, 90};
-
+        for (int i = 0; i < len; i++) {
+            array[i] = random.nextInt(2,10);
+        }
         int GDC = solution.GCD(array, 2, 1);
 
         System.out.println("GDC : "+GDC);
