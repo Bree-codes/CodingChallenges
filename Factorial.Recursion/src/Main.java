a60f631a02fc1e40e979244f0af0fffcb584071e
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-25 05:28:25 +0300

    Factorial program to practice on Recursion

--- Factorial.Recursion/src/Main.java
+++ Factorial.Recursion/src/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
     public static int factorial(int num){
         if(num < 1) return 1;
-        return num* factorial(num-1);
+        return num * factorial(num-1);
     }
 }
