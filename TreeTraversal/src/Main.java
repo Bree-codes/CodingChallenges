b7bb0cd37997734cf775027ded74104a5d259034
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-25 12:25:52 +0300

    Tree traversal;preOrder,postOrder,inOrder

--- TreeTraversal/src/Main.java
+++ TreeTraversal/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Node node7 = new Node(70,null,null);
-        Node node6 = new Node(60, null, null);
-        Node node5 = new Node(50, null, null);
-        Node node4 = new Node(40, null, null);
-        Node node3 = new Node(30, node6, node7);
-        Node node2 = new Node(20, node4, node5);
-        Node node1 = new Node(10, node2, node3);
+        Node node7 = new Node('G',null,null);
+        Node node6 = new Node('F', null, null);
+        Node node5 = new Node('E', null, null);
+        Node node4 = new Node('D', null, null);
+        Node node3 = new Node('C', node6, node7);
+        Node node2 = new Node('B', node4, node5);
+        Node node1 = new Node('A', node2, node3);
 
         System.out.println("pre-order : ");
         node1.preorderTraversal(node1);
--- TreeTraversal/src/Node.java
+++ TreeTraversal/src/Node.java
@@ -1,9 +1,9 @@
 public class Node {
-    int data;
+    char data;
     Node left;
     Node right;
 
-    public Node(int data, Node left, Node right) {
+    public Node(char data, Node left, Node right) {
         this.data = data;
         this.left = left;
         this.right = right;
