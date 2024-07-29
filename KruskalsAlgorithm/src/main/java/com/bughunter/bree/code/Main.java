ce677bb38720e3b3b22bd020f59e2d30888f4e4d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-07-29 15:28:08 +0300

    Let's work on Kruskal's algo for MST

--- KruskalsAlgorithm/src/main/java/com/bughunter/bree/code/Main.java
+++ KruskalsAlgorithm/src/main/java/com/bughunter/bree/code/Main.java
@@ -25,44 +25,6 @@ public class Main {
                 new Edge<>(vertexE, vertexF, 3)
         );
 
-        List<Edge<String>> graph2 = Arrays.asList(
-                new Edge<>(vertexA, vertexB, 7),
-                new Edge<>(vertexA, vertexD, 5),
-                new Edge<>(vertexB, vertexC, 8),
-                new Edge<>(vertexB, vertexE, 7),
-                new Edge<>(vertexB, vertexD, 9),
-                new Edge<>(vertexC, vertexE, 5),
-                new Edge<>(vertexD, vertexE, 15),
-                new Edge<>(vertexD, vertexF, 6),
-                new Edge<>(vertexE, vertexF, 8),
-                new Edge<>(vertexE, vertexG, 9),
-                new Edge<>(vertexF, vertexG, 11)
-        );
-
-        List<Edge<String>> graph3 = Arrays.asList(
-                new Edge<>(vertexA, vertexB, 5),
-                new Edge<>(vertexA, vertexC, 2),
-                new Edge<>(vertexB, vertexC, 3),
-                new Edge<>(vertexB, vertexD, 8),
-                new Edge<>(vertexB, vertexE, 7),
-                new Edge<>(vertexD, vertexE, 6),
-                new Edge<>(vertexC, vertexE, 3)
-        );
-
-        List<Edge<String>> graph4 = Arrays.asList(
-                new Edge<>(vertexA, vertexB, 5),
-                new Edge<>(vertexA, vertexC, 2),
-                new Edge<>(vertexB, vertexC, 4),
-                new Edge<>(vertexB, vertexD, 8),
-                new Edge<>(vertexB, vertexE, 7),
-                new Edge<>(vertexD, vertexE, 2),
-                new Edge<>(vertexC, vertexE, 3)
-        );
-
         new KruskalAlgorithm<>(graph1).run();
-        new KruskalAlgorithm<>(graph2).run();
-        new KruskalAlgorithm<>(graph3).run();
-        new KruskalAlgorithm<>(graph4).run();
-
     }
 }
