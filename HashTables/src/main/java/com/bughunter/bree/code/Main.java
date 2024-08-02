43f18de21b0ca38c73a7248c651179586932ef56
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-08-02 13:22:39 +0300

    Working on hash table data structures

--- HashTables/src/main/java/com/bughunter/bree/code/Main.java
+++ HashTables/src/main/java/com/bughunter/bree/code/Main.java
@@ -1,7 +1,23 @@
 package com.bughunter.bree.code;
 
+import java.util.Hashtable;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Hashtable<Integer,String> table = new Hashtable<>(10);
+        table.put(100,"Bree");
+        table.put(101,"Chebby");
+        table.put(102,"Steve");
+        table.put(122,"Muish");
+        table.put(123,"Kate");
+        table.put(104,"Kiki");
+        table.put(111,"Koko");
+
+        for(Integer key : table.keySet()){
+            System.out.println(key.hashCode() + "\t" + key + "\t " + table.get(key));
+
+        }
+
     }
 }
