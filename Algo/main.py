bac8f6eac99204feea27e7f76c485eb9df30ab80
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2025-02-03 17:43:00 +0300

    algo

---
+++ Algo/main.py
@@ -0,0 +1,18 @@
+def mySqrt(x: int) -> int:
+    if x == 0 or x == 1:
+        return x
+
+    left, right = 0, x
+    result = 0
+
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * mid == x:
+            return mid
+        elif mid * mid < x:
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return result
